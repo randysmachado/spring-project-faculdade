67464b5cd6edaf1456140debc5f408abf5565f7c
Randys Machado <randys.paradise@gmail.com>
2020-12-11 17:45:53 -0300

    initial commit

---
+++ src/test/java/com/spring/faculdade/FaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
